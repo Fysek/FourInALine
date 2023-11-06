62013663d6be83e0ca2d00d7190ebf7b02dbe73d
Mateusz Dyrdol <mateuszdyrdol@gmail.com>
2023-11-06 19:50:11 +0100

    minor changes, added connect_four

--- four_in_a_line.py
+++ four_in_a_line.py
@@ -1,8 +1,9 @@
 class FourInALine:
     def __init__(self):
         self.board = [[' ' for _ in range(5)] for _ in range(5)]
-        self.current_player = 'X'
-        self.player_pieces = {'X': 4, 'O': 4}
+        self.current_player = 'Player 1'
+        self.piece = 'X'
+        self.shared_pieces = 8
 
     def print_board(self):
         for row in self.board:
@@ -12,20 +13,20 @@ class FourInALine:
         return self.board[row][col] == ' '
 
     def make_move(self, row, col):
-        if self.player_pieces[self.current_player] > 0:
+        if self.shared_pieces > 0:
             if self.is_valid_move(row, col):
-                self.board[row][col] = self.current_player
-                self.player_pieces[self.current_player] -= 1
+                self.board[row][col] = self.piece
+                self.shared_pieces -= 1
                 return True
         elif self.is_adjacent(row, col):
             adjacent_pieces = self.get_adjacent_pieces(row, col)
             if adjacent_pieces and all(piece != self.current_player for piece in adjacent_pieces):
-                self.board[row][col] = self.current_player
+                self.board[row][col] = self.piece
                 return True
         return False
 
     def is_adjacent(self, row, col):
-        return any(self.board[i][j] != ' ' for i in range(row-1, row+2) for j in range(col-1, col+2))
+        return any(self.board[i][j] != ' ' for i in range(max(0, row-1), min(5, row+2)) for j in range(max(0, col-1), min(5, col+2)))
 
     def get_adjacent_pieces(self, row, col):
         adjacent_pieces = []
@@ -51,20 +52,24 @@ class FourInALine:
     def is_draw(self):
         return all(cell != ' ' for row in self.board for cell in row) or self.check_winner()
 
+# Example usage:
 game = FourInALine()
 game.print_board()
 
-while not game.is_draw() and not game.check_winner():
-    row = int(input(f'Player {game.current_player}, enter row (0-4): '))
-    col = int(input(f'Player {game.current_player}, enter column (0-4): '))
+while not game.is_draw():
+    row = int(input(f'{game.current_player}, enter row (1-5): ')) - 1
+    col = int(input(f'{game.current_player}, enter column (1-5): ')) - 1
     
     if game.make_move(row, col):
         game.print_board()
-        game.current_player = 'O' if game.current_player == 'X' else 'X'
+        if game.check_winner():
+            break
+        else:
+            game.current_player = 'Player 2' if game.current_player == 'Player 1' else 'Player 1'
     else:
         print("Invalid move. Try again.")
 
 if game.check_winner():
-    print(f"Player {game.current_player} wins!")
+    print(f"{game.current_player} wins!")
 else:
     print("It's a draw!")
